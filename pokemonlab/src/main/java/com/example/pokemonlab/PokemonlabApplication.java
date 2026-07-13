63ac41a9621254d4169156037e05a33d650a50d1
NKJ-1226 <r.nakajima@oplan.co.jp>
2026-07-13 14:05:43 +0900

    Create PokemonLab Spring Boot project

---
+++ pokemonlab/src/main/java/com/example/pokemonlab/PokemonlabApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemonlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonlabApplication.class, args);
+	}
+
+}
---
+++ pokemonlab/src/test/java/com/example/pokemonlab/PokemonlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemonlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
